56786b950511d0bd58c7c7f94895398923eca60d
shailyliu <1277819978@qq.com>
2018-07-06 20:13:14 +0800

    添加测试描述

--- Scripts/test_01.py
+++ Scripts/test_01.py
@@ -4,12 +4,15 @@ class Test_001:
 
     @allure.step(title="第一步测试")
     def test_001(self):
+        @allure.attach("姓名","我是项目描述")
         print("woshiyizhixiaozhu")
 
     @allure.step(title="第二步测试")
+    @allure.attach("写别", "我是性别描述")
     def test_002(self):
         print("我是小刘")
 
     @allure.step(title="第二步测试")
+    @allure.attach("姓", "我是属性描述")
     def test_003(self):
         print("闹闹")
