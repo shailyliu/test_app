09627720701a8450144083a08008b658c55ce47f
shailyliu <1277819978@qq.com>
2018-07-06 20:07:21 +0800

    添加测试步骤

--- Scripts/test_01.py
+++ Scripts/test_01.py
@@ -1,12 +1,15 @@
-
+import allure
 
 class Test_001:
 
+    @allure.step(title="第一步测试")
     def test_001(self):
         print("woshiyizhixiaozhu")
 
+    @allure.step(title="第二步测试")
     def test_002(self):
         print("我是小刘")
 
+    @allure.step(title="第二步测试")
     def test_003(self):
         print("闹闹")
