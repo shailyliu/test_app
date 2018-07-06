9a55e3113291becdf2071f2a1f3f87fb824cf117
shailyliu <1277819978@qq.com>
2018-07-06 20:35:55 +0800

    updata ceshimiaoshu

--- Scripts/test_01.py
+++ Scripts/test_01.py
@@ -4,15 +4,16 @@ class Test_001:
 
     @allure.step(title="第一步测试")
     def test_001(self):
-        @allure.attach("姓名","我是项目描述")
+        allure.attach("姓名","我是项目描述")
         print("woshiyizhixiaozhu")
 
     @allure.step(title="第二步测试")
-    @allure.attach("写别", "我是性别描述")
     def test_002(self):
+        allure.attach("写别", "我是性别描述")
         print("我是小刘")
 
     @allure.step(title="第二步测试")
-    @allure.attach("姓", "我是属性描述")
+
     def test_003(self):
+        allure.attach("姓", "我是属性描述")
         print("闹闹")
